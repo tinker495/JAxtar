b04041c8cb7dc8e6291a9f254e55612777f31ace
tinker495 <wjdrbtjr495@gmail.com>
2024-09-18 12:33:14 +0900

    fix simple heur

--- heuristic/rubikscube_heuristic.py
+++ heuristic/rubikscube_heuristic.py
@@ -5,9 +5,14 @@ from puzzle.rubikscube import RubiksCube
 
 class RubiksCubeHeuristic:
     puzzle: RubiksCube
+    heur_modify: float
 
     def __init__(self, puzzle: RubiksCube):
         self.puzzle = puzzle
+        if puzzle.size % 2 == 0:
+            self.heur_modify = 0
+        else:
+            self.heur_modify = 1 / (puzzle.size ** 2)
 
     def distance(self, current: RubiksCube.State, target: RubiksCube.State) -> float:
         """
@@ -15,5 +20,6 @@ class RubiksCubeHeuristic:
         """
         current_faces = current.faces
         target_faces = target.faces
-        equal_faces = 1 - jnp.equal(current_faces, target_faces).mean(1)
+        equal_faces = 1 - (jnp.equal(current_faces, target_faces).mean(1) - self.heur_modify) / (1 - self.heur_modify)
+        # center of faces are not considered
         return equal_faces.sum()
