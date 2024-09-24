9371ff8735d3483daa688fa6f776ed6fe5a99fc6
tinker495 <wjdrbtjr495@gmail.com>
2024-09-24 22:31:57 +0900

    save models

--- train_davi.py
+++ train_davi.py
@@ -14,7 +14,7 @@ from heuristic.DAVI.davi import create_shuffled_path, davi_builder
 @click.option("--steps", type=int, default=10000)
 @click.option("--key", type=int, default=0)
 @click.option("--debug", is_flag=True, help="Debug mode")
-def train_davi(puzzle, puzzle_size: int, steps: int, key: int, debug: bool):
+def train_davi(puzzle: str, puzzle_size: int, steps: int, key: int, debug: bool):
     if debug:
         #disable jit
         print("Disabling JIT")
@@ -23,12 +23,14 @@ def train_davi(puzzle, puzzle_size: int, steps: int, key: int, debug: bool):
         puzzle_size = default_puzzle_sizes[puzzle]
     else:
         puzzle_size = int(puzzle_size)
-    puzzle, heuristic = puzzle_dict_nn[puzzle](puzzle_size)
+    puzzle_name = puzzle
+    puzzle, heuristic = puzzle_dict_nn[puzzle_name](puzzle_size)
+    heuristic.save_model(f"heuristic/DAVI/neuralheuristic/params/{puzzle_name}_{puzzle_size}.pkl")
 
     heuristic_fn = heuristic.param_distance
     heuristic_params = heuristic.params
 
-    davi_fn, opt_state = davi_builder(puzzle, int(1e2), int(1e3), 200, heuristic_fn, heuristic_params)
+    davi_fn, opt_state = davi_builder(puzzle, int(1e3), int(1e3), 200, heuristic_fn, heuristic_params)
 
     key = jax.random.PRNGKey(key)
     pbar = trange(steps)
@@ -36,6 +38,9 @@ def train_davi(puzzle, puzzle_size: int, steps: int, key: int, debug: bool):
         key, subkey = jax.random.split(key)
         heuristic_params, opt_state, loss, mean_target_heuristic = davi_fn(subkey, heuristic_params, opt_state)
         pbar.set_description(f"Loss: {loss:5.4f}, Mean Target Heuristic: {mean_target_heuristic:4.1f}")
+
+    heuristic.params = heuristic_params
+    heuristic.save_model(f"heuristic/DAVI/neuralheuristic/params/{puzzle_name}_{puzzle_size}.pkl")
 
 if __name__ == "__main__":
     train_davi()
