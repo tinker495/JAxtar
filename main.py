d0ff9cf62c1754b1fe9d63c7c4c4ab2cdb0a1646
tinker495 <wjdrbtjr495@gmail.com>
2024-09-24 20:39:36 +0900

    fixed

--- main.py
+++ main.py
@@ -3,36 +3,9 @@ import jax.numpy as jnp
 import click
 import time
 
-from functools import partial
-from puzzle.slidepuzzle import SlidePuzzle
-from puzzle.lightsout import LightsOut
-from puzzle.rubikscube import RubiksCube
-from puzzle.maze import Maze
 from JAxtar.hash import HashTable
 from JAxtar.astar import astar_builder
-from heuristic.slidepuzzle_heuristic import SlidePuzzleHeuristic
-from heuristic.DAVI.neuralheuristic.slidepuzzle_neuralheuristic import SlidePuzzleNeuralHeuristic
-from heuristic.DAVI.neuralheuristic.lightsout_neuralheuristic import LightsOutNeuralHeuristic
-from heuristic.lightsout_heuristic import LightsOutHeuristic
-from heuristic.rubikscube_heuristic import RubiksCubeHeuristic
-from heuristic.maze_heuristic import MazeHeuristic
-
-defualt_puzzle_sizes = {
-    "n-puzzle": 4,
-    "n-puzzle-nn": 4,
-    "lightsout": 7,
-    "lightsout-nn": 7,
-    "rubikscube": 3,
-    "maze": 20
-}
-puzzle_dict = {
-    "n-puzzle": lambda n: (SlidePuzzle(n), SlidePuzzleHeuristic(SlidePuzzle(n)).distance),
-    "n-puzzle-nn": lambda n: (SlidePuzzle(n), SlidePuzzleNeuralHeuristic(SlidePuzzle(n)).distance),
-    "lightsout": lambda n: (LightsOut(n), LightsOutHeuristic(LightsOut(n)).distance),
-    "lightsout-nn": lambda n: (LightsOut(n), LightsOutNeuralHeuristic(LightsOut(n)).distance),
-    "rubikscube": lambda n: (RubiksCube(n), RubiksCubeHeuristic(RubiksCube(n)).distance),
-    "maze": lambda n: (Maze(n), MazeHeuristic(Maze(n)).distance)
-}
+from puzzle_config import default_puzzle_sizes, puzzle_dict, puzzle_dict_nn
 
 def human_format(num):
     num = float('{:.3g}'.format(num))
@@ -53,16 +26,26 @@ def human_format(num):
 @click.option("--vmap_size", default=1, help="Size for the vmap")
 @click.option("--debug", is_flag=True, help="Debug mode")
 @click.option("--profile", is_flag=True, help="Profile mode")
-def main(puzzle, puzzle_size, max_node_size, batch_size, astar_weight, start_state_seed, seed, vmap_size, debug, profile):
+@click.option("--nn", is_flag=True, help="Use neural heuristic")
+def main(puzzle, puzzle_size, max_node_size, batch_size, astar_weight, start_state_seed, seed, vmap_size, debug, profile, nn):
     if debug:
         #disable jit
         print("Disabling JIT")
         jax.config.update('jax_disable_jit', True)
     if puzzle_size == "default":
-        puzzle_size = defualt_puzzle_sizes[puzzle]
+        puzzle_size = default_puzzle_sizes[puzzle]
     else:
         puzzle_size = int(puzzle_size)
-    puzzle, heuristic_fn = puzzle_dict[puzzle](puzzle_size)
+    if nn:
+        try:
+            puzzle, heuristic = puzzle_dict_nn[puzzle](puzzle_size)
+        except KeyError:
+            print("Neural heuristic not available for this puzzle")
+            exit(1)
+    else:
+        puzzle, heuristic = puzzle_dict[puzzle](puzzle_size)
+
+    heuristic_fn = heuristic.distance
 
     max_node_size = int(max_node_size)
     batch_size = int(batch_size)
---
+++ puzzle_config.py
@@ -0,0 +1,31 @@
+from puzzle.slidepuzzle import SlidePuzzle
+from puzzle.lightsout import LightsOut
+from puzzle.rubikscube import RubiksCube
+from puzzle.maze import Maze
+from heuristic.slidepuzzle_heuristic import SlidePuzzleHeuristic
+from heuristic.DAVI.neuralheuristic.slidepuzzle_neuralheuristic import SlidePuzzleNeuralHeuristic
+from heuristic.DAVI.neuralheuristic.lightsout_neuralheuristic import LightsOutNeuralHeuristic
+from heuristic.lightsout_heuristic import LightsOutHeuristic
+from heuristic.rubikscube_heuristic import RubiksCubeHeuristic
+from heuristic.maze_heuristic import MazeHeuristic
+
+default_puzzle_sizes = {
+    "n-puzzle": 4,
+    "lightsout": 7,
+    "rubikscube": 3,
+    "maze": 20
+}
+
+puzzle_dict = {
+    "n-puzzle": lambda n: (SlidePuzzle(n), SlidePuzzleHeuristic(SlidePuzzle(n))),
+    "lightsout": lambda n: (LightsOut(n), LightsOutHeuristic(LightsOut(n))),
+    "rubikscube": lambda n: (RubiksCube(n), RubiksCubeHeuristic(RubiksCube(n))),
+    "maze": lambda n: (Maze(n), MazeHeuristic(Maze(n)))
+}
+
+puzzle_dict_nn = {
+    "n-puzzle": lambda n: (SlidePuzzle(n), SlidePuzzleNeuralHeuristic(SlidePuzzle(n))),
+    "lightsout": lambda n: (LightsOut(n), LightsOutNeuralHeuristic(LightsOut(n))),
+    #"rubikscube": lambda n: (RubiksCube(n), RubiksCubeNeuralHeuristic(RubiksCube(n)).distance),
+    #"maze": lambda n: (Maze(n), MazeNeuralHeuristic(Maze(n)).distance)
+}
