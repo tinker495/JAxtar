ca7fefd7b5ead3678f1dce395e05aedc5d3bbe4a
tink495 <wjdrbtjr495@gmail.com>
2024-09-24 07:00:48 +0000

    testing davi

---
+++ train_davi.py
@@ -0,0 +1,17 @@
+import jax
+import jax.numpy as jnp
+import chex
+
+from puzzle.puzzle_base import Puzzle
+from puzzle.slidepuzzle import SlidePuzzle
+from heuristic.DAVI.davi import create_shuffled_path
+
+def train_davi(puzzle: Puzzle, steps: int):
+    """
+    Train DAVI on the sliding puzzle problem.
+    """
+    print(create_shuffled_path(puzzle, 1000, 20, jax.random.PRNGKey(0)))
+
+if __name__ == "__main__":
+    puzzle = SlidePuzzle(4)
+    train_davi(puzzle, 10)
