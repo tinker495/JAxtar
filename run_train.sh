4ba636ed4f27513beaeb27cdb9ddc7a7a935d858
tinker495 <wjdrbtjr495@gmail.com>
2024-11-27 19:15:11 +0900

    add test sh

---
+++ run_train.sh
@@ -0,0 +1,3 @@
+tmux new -s train_rubikscube -d "python train_davi.py --puzzle rubikscube --shuffle_length 30"
+#tmux new -s train_lightsout -d "python train_davi.py --puzzle lightsout --shuffle_length 30"
+#tmux new -s train_slidepuzzle -d "python train_davi.py --puzzle slidepuzzle --shuffle_length 500"
