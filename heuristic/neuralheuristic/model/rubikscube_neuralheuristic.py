9f99951c9c1ffc1dc2be6e2fdebe6fb489b68782
tinker495 <wjdrbtjr495@gmail.com>
2024-11-28 15:17:33 +0900

    just using batchnorm

--- heuristic/neuralheuristic/model/rubikscube_neuralheuristic.py
+++ heuristic/neuralheuristic/model/rubikscube_neuralheuristic.py
@@ -8,39 +8,11 @@ from puzzle.rubikscube import RubiksCube
 NODE_SIZE = 256
 
 
-# Simba Residual Block
-class ResBlock(nn.Module):
-    node_size: int
-
-    @nn.compact
-    def __call__(self, x0):
-        x = nn.LayerNorm()(x0)
-        x = nn.Dense(self.node_size)(x)
-        x = nn.relu(x)
-        x = nn.Dense(self.node_size)(x)
-        return x + x0
-
-
-class Model(nn.Module):
-    @nn.compact
-    def __call__(self, x):
-        x = nn.Dense(5000)(x)
-        x = nn.tanh(x)
-        x = nn.Dense(1000)(x)
-        x = ResBlock(1000)(x)
-        x = ResBlock(1000)(x)
-        x = ResBlock(1000)(x)
-        x = ResBlock(1000)(x)
-        x = nn.LayerNorm()(x)
-        x = nn.Dense(1)(x)
-        return x
-
-
 class RubiksCubeNeuralHeuristic(NeuralHeuristicBase):
     base_xy: chex.Array  # The coordinates of the numbers in the puzzle
 
     def __init__(self, puzzle: RubiksCube, init_params: bool = True):
-        super().__init__(puzzle, Model(), init_params=init_params)
+        super().__init__(puzzle, init_params=init_params)
 
     def pre_process(self, current: RubiksCube.State, target: RubiksCube.State) -> chex.Array:
         flatten_face = current.faces.flatten()  # (3,3,6) -> (54,)
