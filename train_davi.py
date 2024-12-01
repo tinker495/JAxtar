9625ffcaad27f69a2e0c50f3fc881a447e25a2ad
tinker495 <wjdrbtjr495@gmail.com>
2024-12-01 10:43:00 +0900

    fixed

--- train_davi.py
+++ train_davi.py
@@ -1,14 +1,16 @@
 from copy import deepcopy
 from datetime import datetime
 from typing import Any
-
+import math
 import click
 import jax
 import jax.numpy as jnp
 import numpy as np
+import optax
 import tensorboardX
 from tqdm import trange
 
+from heuristic.neuralheuristic.neuralheuristic_base import NeuralHeuristicBase
 from heuristic.neuralheuristic.davi import davi_builder, get_dataset_builder
 from puzzle_config import default_puzzle_sizes, puzzle_dict, puzzle_heuristic_dict_nn
 
@@ -30,7 +32,7 @@ def soft_update(new_tensors: PyTree, old_tensors: PyTree, tau: float):
     help="Puzzle to solve",
 )
 @click.option("--puzzle_size", default="default", type=str, help="Size of the puzzle")
-@click.option("--steps", type=int, default=100000)
+@click.option("--steps", type=int, default=20000)
 @click.option("--shuffle_length", type=int, default=30)
 @click.option("--key", type=int, default=0)
 @click.option("--reset", is_flag=True, help="Reset the target heuristic params")
@@ -54,7 +56,7 @@ def train_davi(
         puzzle_size = int(puzzle_size)
     puzzle_name = puzzle
     puzzle = puzzle_dict[puzzle_name](puzzle_size)
-    heuristic = puzzle_heuristic_dict_nn[puzzle_name](puzzle_size, puzzle, reset)
+    heuristic : NeuralHeuristicBase = puzzle_heuristic_dict_nn[puzzle_name](puzzle_size, puzzle, reset)
 
     # Setup tensorboard logging
     log_dir = f"runs/{puzzle_name}_{puzzle_size}_{datetime.now().strftime('%Y%m%d-%H%M%S')}"
@@ -65,12 +67,15 @@ def train_davi(
     target_heuristic_params = deepcopy(heuristic_params)
     key = jax.random.PRNGKey(np.random.randint(0, 1000000) if key == 0 else key)
     key, subkey = jax.random.split(key)
-    dataset_size = int(3e5)
-    shuffle_parallel = int(3e4 // shuffle_length)
+    dataset_size = int(1e6)
     dataset_minibatch_size = 10000
-    minibatch_size = 1024 * 8
+    shuffle_parallel = int(math.ceil(dataset_minibatch_size / shuffle_length))
+    minibatch_size = 1000
 
-    davi_fn, opt_state = davi_builder(minibatch_size, heuristic_fn, heuristic_params)
+    optimizer = optax.adabelief(1e-4)
+    opt_state = optimizer.init(heuristic_params)
+
+    davi_fn = davi_builder(minibatch_size, heuristic_fn, optimizer)
     get_datasets = get_dataset_builder(
         puzzle,
         heuristic.pre_process,
@@ -82,39 +87,44 @@ def train_davi(
     )
 
     pbar = trange(steps)
-
+    dataset = get_datasets(
+        target_heuristic_params,
+        subkey,
+    )
+    target_heuristic = dataset[1]
+    mean_target_heuristic = jnp.mean(target_heuristic)
+    writer.add_scalar("Mean Target Heuristic", mean_target_heuristic, 0)
+    writer.add_histogram("Target Heuristic", target_heuristic, 0)
     for i in pbar:
         key, subkey = jax.random.split(key)
-        dataset = get_datasets(
-            target_heuristic_params,
-            subkey,
-        )
-        target_heuristic = dataset[1]
-        mean_target_heuristic = jnp.mean(target_heuristic)
-        writer.add_scalar("Mean Target Heuristic", mean_target_heuristic, i)
-        writer.add_histogram("Target Heuristic", target_heuristic, i)
-        heuristic_params, opt_state, loss, mean_abs_diff = davi_fn(
+        heuristic_params, opt_state, loss, mean_abs_diff, diffs = davi_fn(
             key, dataset, heuristic_params, opt_state
         )
         pbar.set_description(
             f"loss: {loss:.4f}, mean_abs_diff: {mean_abs_diff:.2f}, mean_target_heuristic: {mean_target_heuristic:.4f}"
         )
+        if i % 5 == 0:
+            writer.add_histogram("Diff", diffs, i)
 
-        # Log metrics to tensorboard
-        writer.add_scalar("Loss", loss, i)
-        writer.add_scalar("Mean Abs Diff", mean_abs_diff, i)
+            # Log metrics to tensorboard
+            writer.add_scalar("Loss", loss, i)
+            writer.add_scalar("Mean Abs Diff", mean_abs_diff, i)
 
-
-        if (
-            ((i % 1000 == 0 or (mean_target_heuristic < shuffle_length // 10 and i % 100 == 0)) and i != 0)
-            and loss <= 0.05
-        ):
-            target_heuristic_params = deepcopy(heuristic_params)
-            heuristic.params = heuristic_params
-            heuristic.save_model(
-                f"heuristic/neuralheuristic/model/params/{puzzle_name}_{puzzle_size}.pkl"
-            )
-
+        if (i % 100 == 0 and i != 0) and loss <= 0.05:
+                heuristic.params = heuristic_params
+                heuristic.save_model(
+                    f"heuristic/neuralheuristic/model/params/{puzzle_name}_{puzzle_size}.pkl"
+                )
+                target_heuristic_params = deepcopy(heuristic_params)
+                dataset = get_datasets(
+                    target_heuristic_params,
+                    subkey,
+                )
+                target_heuristic = dataset[1]
+                mean_target_heuristic = jnp.mean(target_heuristic)
+                writer.add_scalar("Mean Target Heuristic", mean_target_heuristic, i)
+                writer.add_histogram("Target Heuristic", target_heuristic, i)
+                opt_state = optimizer.init(heuristic_params) # reset optimizer state
 
 if __name__ == "__main__":
     train_davi()
