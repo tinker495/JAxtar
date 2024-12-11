032913bc8489359ed470e196676319889014b3ac
tinker495 <wjdrbtjr495@gmail.com>
2024-12-11 12:01:39 +0900

    cross target
    Former-commit-id: ff16278316ace301356625cc4df391a67320e79a
    Former-commit-id: c94d6f384544de155948e1071e39718deb6c6e9a

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,3 @@
-tmux new -s train -d "python train_davi.py --puzzle rubikscube --shuffle_length 30 --reset"
+tmux new -s train -d "python train_davi.py --puzzle rubikscube --shuffle_length 30 --reset -l 0.2"
 #tmux new -s train -d "python train_davi.py --puzzle lightsout --shuffle_length 30 --reset"
 #tmux new -s train -d "python train_davi.py --puzzle n-puzzle --shuffle_length 500"
