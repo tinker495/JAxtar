06ba47a88a125831a2ca6516b339a14eec37efcd
tinker495 <wjdrbtjr495@gmail.com>
2024-12-11 14:37:50 +0900

    add qlearnings
    Former-commit-id: 78b1ba600ad342afd8fb9df813e05b71ff8db2f7
    Former-commit-id: 4ba22c50afb4cb04a70905cf55db8a1daadb46f2

---
+++ heuristic/neuralheuristic/__init__.py
@@ -0,0 +1,4 @@
+from heuristic.neuralheuristic.model.lightsout_neuralheuristic import LightsOutNeuralHeuristic
+from heuristic.neuralheuristic.model.rubikscube_neuralheuristic import RubiksCubeNeuralHeuristic
+from heuristic.neuralheuristic.model.slidepuzzle_neuralheuristic import SlidePuzzleNeuralHeuristic
+
---
+++ qfunction/__init__.py
@@ -0,0 +1,5 @@
+from qfunction.lightsout_q import LightsOutQ
+from qfunction.maze_q import MazeQ
+from qfunction.rubikscube_q import RubiksCubeQ
+from qfunction.slidepuzzle_q import SlidePuzzleQ
+from qfunction.neuralq import LightsOutNeuralQ, RubiksCubeNeuralQ, SlidePuzzleNeuralQ
