6e1d5f4778afbe1397ee59efa2039c5b032da86d
tinker495 <wjdrbtjr495@gmail.com>
2024-12-14 23:43:28 +0900

    asdf

--- qfunction/neuralq/model/lightsout_neuralq.py
+++ qfunction/neuralq/model/lightsout_neuralq.py
@@ -14,7 +14,7 @@ class ConvResBlock(nn.Module):
     strides: int
 
     @nn.compact
-    def __call__(self, x0, training: bool = True):
+    def __call__(self, x0, training=False):
         x = nn.Conv(self.filters, self.kernel_size, strides=self.strides, padding="SAME")(x0)
         x = nn.BatchNorm()(x, use_running_average=not training)
         x = nn.relu(x)
@@ -23,27 +23,36 @@ class ConvResBlock(nn.Module):
         return nn.relu(x + x0)
 
 
+class ResBlock(nn.Module):
+    node_size: int
+
+    @nn.compact
+    def __call__(self, x0, training=False):
+        x = nn.Dense(self.node_size)(x0)
+        x = nn.BatchNorm()(x, use_running_average=not training)
+        x = nn.relu(x)
+        x = nn.Dense(self.node_size)(x)
+        x = nn.BatchNorm()(x, use_running_average=not training)
+        return nn.relu(x + x0)
+
+
 class Model(nn.Module):
     action_size: int
 
     @nn.compact
-    def __call__(self, x, training: bool = True):
+    def __call__(self, x, training=False):
         # [4, 4, 1] -> conv
-        x = nn.Conv(256, (3, 3), strides=1, padding="SAME")(x)
+        x = nn.Conv(64, (3, 3), strides=1, padding="SAME")(x)
         x = nn.BatchNorm()(x, use_running_average=not training)
         x = nn.relu(x)
-        x = nn.Conv(32, (3, 3), strides=1, padding="SAME")(x)
+        x = ConvResBlock(64, (3, 3), strides=1)(x, training)
+        x = jnp.reshape(x, (x.shape[0], -1))
+        x = nn.Dense(1024)(x)
         x = nn.BatchNorm()(x, use_running_average=not training)
         x = nn.relu(x)
-        x = ConvResBlock(32, (3, 3), strides=1)(x, training)
-        x = ConvResBlock(32, (3, 3), strides=1)(x, training)
-        value_feature = jnp.reshape(x, (x.shape[0], -1))
-        value = nn.Dense(1)(value_feature)
-        adv = nn.Conv(1, (1, 1), strides=1, padding="SAME")(x)
-        adv = jnp.reshape(adv, (x.shape[0], -1))
-        q = value + adv - jnp.mean(adv, axis=1, keepdims=True)
-        chex.assert_shape(q, (None, self.action_size))
-        return q
+        x = ResBlock(1024)(x, training)
+        x = nn.Dense(self.action_size)(x)
+        return x
 
 
 class LightsOutNeuralQ(NeuralQFunctionBase):
