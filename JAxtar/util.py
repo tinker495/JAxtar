5457ace9ea1e849132e0425f805780510b9dba2f
tinker495 <wjdrbtjr495@gmail.com>
2025-01-06 10:31:06 +0900

    util at set

---
+++ JAxtar/util.py
@@ -0,0 +1,31 @@
+import chex
+import jax
+import jax.numpy as jnp
+
+
+def set_array_as_condition(
+    array: chex.Array, condition: chex.Array, insert_value: chex.Array, *indexs
+) -> chex.Array:
+    """
+    Set the value of the array at the given indexs to the insert_value if the condition is true,
+    otherwise keep the original value.
+    """
+    return array.at[indexs].set(
+        jnp.where(
+            jnp.reshape(condition, (-1,) + (insert_value.ndim - 1) * (1,)),
+            insert_value,
+            array[indexs],
+        )
+    )
+
+
+def set_tree_as_condition(
+    tree: chex.Array, condition: chex.Array, insert_tree: chex.Array, *indexs
+) -> chex.Array:
+    """
+    Set the value of the tree at the given indexs to the insert_tree if the condition is true,
+    otherwise keep the original value.
+    """
+    return jax.tree_map(
+        lambda t, i: set_array_as_condition(t, condition, i, *indexs), tree, insert_tree
+    )
