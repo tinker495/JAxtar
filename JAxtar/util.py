29cb8bee909af931a2d98652f368829850d3654a
tinker495 <wjdrbtjr495@gmail.com>
2025-01-06 10:46:25 +0900

    rm at sets

--- JAxtar/util.py
+++ JAxtar/util.py
@@ -3,6 +3,20 @@ import jax
 import jax.numpy as jnp
 
 
+def set_array(array: chex.Array, insert_value: chex.Array, *indexs) -> chex.Array:
+    """
+    Set the value of the array at the given indexs to the insert_value.
+    """
+    return array.at[indexs].set(insert_value)
+
+
+def set_tree(tree: chex.Array, insert_value: chex.Array, *indexs) -> chex.Array:
+    """
+    Set the value of the tree at the given indexs to the insert_value.
+    """
+    return jax.tree_map(lambda t, i: set_array(t, i, *indexs), tree, insert_value)
+
+
 def set_array_as_condition(
     array: chex.Array, condition: chex.Array, insert_value: chex.Array, *indexs
 ) -> chex.Array:
