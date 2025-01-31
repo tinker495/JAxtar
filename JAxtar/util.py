a84bffc8987d66cdf20323d08414f69eca45a825
tinker495 <wjdrbtjr495@gmail.com>
2025-01-31 17:26:59 +0900

    fixing pytests

--- JAxtar/util.py
+++ JAxtar/util.py
@@ -14,7 +14,7 @@ def set_tree(tree: chex.Array, insert_value: chex.Array, *indexs) -> chex.Array:
     """
     Set the value of the tree at the given indexs to the insert_value.
     """
-    return jax.tree_map(lambda t, i: set_array(t, i, *indexs), tree, insert_value)
+    return jax.tree_util.tree_map(lambda t, i: set_array(t, i, *indexs), tree, insert_value)
 
 
 def set_array_as_condition(
@@ -40,6 +40,6 @@ def set_tree_as_condition(
     Set the value of the tree at the given indexs to the insert_tree if the condition is true,
     otherwise keep the original value.
     """
-    return jax.tree_map(
+    return jax.tree_util.tree_map(
         lambda t, i: set_array_as_condition(t, condition, i, *indexs), tree, insert_tree
     )
