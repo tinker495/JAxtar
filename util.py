8b1a1a6c34687cc3b7785a70ec368321a1fef3ad
tinker495 <wjdrbtjr495@gmail.com>
2025-02-06 20:36:24 +0900

    cleaning!
    Former-commit-id: cf60e9fc8d1d37134c1c7b93575d5241a644ce26
    Former-commit-id: 596c2ed56152015b3a209fd7cab6ddad78b5bad6

--- util.py
+++ util.py
@@ -4,7 +4,8 @@ from itertools import islice
 import jax
 import jax.numpy as jnp
 
-from JAxtar.hash import HashTable
+from JAxtar.search_base import HashTableIdx_HeapValue, SearchResult
+from JAxtar.util import set_tree
 from puzzle.puzzle_base import Puzzle
 
 
@@ -19,12 +20,39 @@ def human_format(num):
     )
 
 
+def vmapping_init_target(puzzle: Puzzle, vmap_size: int, start_state_seeds: list[int]):
+    start_state_seed = start_state_seeds[0]
+    states, targets = puzzle.get_init_target_state_pair(jax.random.PRNGKey(start_state_seed))
+    states = jax.tree_util.tree_map(
+        lambda x: jnp.tile(x, (vmap_size,) + (1,) * len(x.shape[1:])), states[jnp.newaxis, ...]
+    )
+    targets = jax.tree_util.tree_map(
+        lambda x: jnp.tile(x, (vmap_size,) + (1,) * len(x.shape[1:])), targets[jnp.newaxis, ...]
+    )
+
+    if len(start_state_seeds) > 1:
+        for i, start_state_seed in enumerate(start_state_seeds[1:vmap_size]):
+            new_state, new_target = puzzle.get_init_target_state_pair(
+                jax.random.PRNGKey(start_state_seed)
+            )
+            states = set_tree(
+                states,
+                new_state,
+                i + 1,
+            )
+            targets = set_tree(
+                targets,
+                new_target,
+                i + 1,
+            )
+    return states, targets
+
+
 def vmapping_search(
     puzzle: Puzzle,
     search_result_build: callable,
     star_fn: callable,
     vmap_size: int,
-    batch_size: int,
     show_compile_time: bool = False,
 ):
     """
@@ -36,19 +64,20 @@ def vmapping_search(
     empty_states = jax.tree_util.tree_map(
         lambda x: jnp.tile(x, (vmap_size,) + (1,) * len(x.shape[1:])), empty_states
     )
-    empty_states_batched, filled = jax.vmap(
-        lambda x: HashTable.make_batched(puzzle.State, x[jnp.newaxis, ...], batch_size), in_axes=0
-    )(empty_states)
-    vmapped_star = jax.jit(jax.vmap(star_fn, in_axes=(None, 0, 0, 0)))
+    vmapped_star = jax.jit(jax.vmap(star_fn, in_axes=(None, 0, 0)))
     if show_compile_time:
         print("initializing vmapped jit")
         start = time.time()
-    vmapped_star(inital_search_result, empty_states_batched, filled, empty_states)
+    vmapped_star(inital_search_result, empty_states, empty_states)
     if show_compile_time:
         end = time.time()
         print(f"Compile Time: {end - start:6.2f} seconds")
         print("JIT compiled\n\n")
     return vmapped_star
+
+
+def vmapping_get_state(search_result: SearchResult, idx: HashTableIdx_HeapValue):
+    return jax.vmap(SearchResult.get_state, in_axes=(None, 0))(search_result, idx)
 
 
 def window(seq, n=2):
