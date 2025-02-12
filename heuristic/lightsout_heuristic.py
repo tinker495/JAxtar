90cf51c11f47fca23ccfa988b818c0812ba72288
tinker495 <wjdrbtjr495@gmail.com>
2025-02-12 16:12:29 +0900

    add lightsout

--- heuristic/lightsout_heuristic.py
+++ heuristic/lightsout_heuristic.py
@@ -8,12 +8,13 @@ class LightsOutHeuristic(Heuristic):
     def __init__(self, puzzle: LightsOut):
         super().__init__(puzzle)
 
-    def distance(self, current: LightsOut.State, target: LightsOut.State) -> float:
+    def distance(self, solve_config: LightsOut.SolveConfig, current: LightsOut.State) -> float:
         """
         Get distance between current state and target state.
         """
         neq_state = jnp.not_equal(
-            self.puzzle.from_uint8(current.board), self.puzzle.from_uint8(target.board)
+            self.puzzle.from_uint8(current.board),
+            self.puzzle.from_uint8(solve_config.TargetState.board),
         )
         sum_neq_state = jnp.sum(neq_state)
         return sum_neq_state / 5 * 2
