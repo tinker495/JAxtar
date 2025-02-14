d4688fe37e563760c041543ecdd744f82ff8b20e
tinker495 <wjdrbtjr495@gmail.com>
2025-02-14 17:14:58 +0900

    add train option

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import click
 
-from cli import astar, human_play, qstar
+from cli import astar, human_play, qstar, train
 
 
 @click.group()
@@ -12,4 +12,5 @@ if __name__ == "__main__":
     main.add_command(human_play)
     main.add_command(astar)
     main.add_command(qstar)
+    main.add_command(train)
     main()
