750ff1b28755d1d4802f20b6f380080cc41f0b9d
tinker495 <wjdrbtjr495@gmail.com>
2025-02-15 15:14:53 +0900

    move train commands
    Former-commit-id: cd740b9596718ee229e7b683e5298e2aaac0fe14
    Former-commit-id: 3e9d9582c008074da358a6ef8fa47553f977a3b6

--- cli/train_commands/__init__.py
+++ cli/train_commands/__init__.py
@@ -1,6 +1,12 @@
 import click
 
+from .train_command import davi, qlearning
+
 
 @click.group()
 def train():
     pass
+
+
+train.add_command(davi, name="davi")
+train.add_command(qlearning, name="qlearning")
