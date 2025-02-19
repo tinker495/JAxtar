f2c84b776b207e1a77da16e1d7bd18b8e3a47596
tinker495 <wjdrbtjr495@gmail.com>
2025-02-19 17:46:45 +0900

    update optimized world model
    Former-commit-id: cfea465edee32fe42e643310f3b5d5e2f8d66581
    Former-commit-id: cd8f4e8876f40638aef070ce7037728d8afff41a

--- puzzle/world_model/__init__.py
+++ puzzle/world_model/__init__.py
@@ -1,6 +1,8 @@
 from .model.rubikscube_world_model import (
     RubiksCubeWorldModel,
     RubiksCubeWorldModel_test,
+    RubiksCubeWorldModelOptimized,
+    RubiksCubeWorldModelOptimized_test,
 )
 from .model.sokoban_world_model import SokobanWorldModel, SokobanWorldModelOptimized
 from .world_model_puzzle_base import WorldModelPuzzleBase
