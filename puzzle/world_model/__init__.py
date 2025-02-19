9e1ed3e03c0ad9a3b2f816ea4d620eba11df6dce
tinker495 <wjdrbtjr495@gmail.com>
2025-02-19 14:04:14 +0900

    optimized ver

--- puzzle/world_model/__init__.py
+++ puzzle/world_model/__init__.py
@@ -1,3 +1,6 @@
-from .model.rubikscube_world_model import RubiksCubeWorldModel
-from .model.sokoban_world_model import SokobanWorldModel
+from .model.rubikscube_world_model import (
+    RubiksCubeWorldModel,
+    RubiksCubeWorldModel_test,
+)
+from .model.sokoban_world_model import SokobanWorldModel, SokobanWorldModelOptimized
 from .world_model_puzzle_base import WorldModelPuzzleBase
