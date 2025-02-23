1f72921e5511b0feb4be4967b1967b3278843292
tinker495 <wjdrbtjr495@gmail.com>
2025-02-23 12:44:58 +0900

    using decay
    Former-commit-id: 6d9494e41374e70977cfe782075b73ec848a341c
    Former-commit-id: 25ac94e1f301cf5871d81c9b8562a166ff7c0fc7

--- cli/train_commands/world_model_train_command.py
+++ cli/train_commands/world_model_train_command.py
@@ -34,7 +34,7 @@ def setup_logging(world_model_name: str) -> tensorboardX.SummaryWriter:
 def setup_optimizer(params: PyTree) -> optax.OptState:
     optimizer = optax.chain(
         optax.clip_by_global_norm(10.0),  # Clip gradients to a maximum global norm of 1.0
-        optax.adam(1e-3, nesterov=True),
+        optax.adamw(1e-3, nesterov=True, weight_decay=1e-4),
     )
     return optimizer, optimizer.init(params)
 
