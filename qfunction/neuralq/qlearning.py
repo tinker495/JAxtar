c8d264ad3074748197efe8050c804dd001c92d25
tinker495 <wjdrbtjr495@gmail.com>
2025-02-25 11:00:06 +0900

    fix batchnorm training
    Former-commit-id: 51ebff7b2ed08b6c5912a2c94638ed77945b2197
    Former-commit-id: 0aa97a5004016ff90f960d5e204328635496c354

--- qfunction/neuralq/qlearning.py
+++ qfunction/neuralq/qlearning.py
@@ -16,26 +16,22 @@ def qlearning_builder(
     optimizer: optax.GradientTransformation,
 ):
     def qlearning_loss(
-        heuristic_params: jax.tree_util.PyTreeDef,
+        q_params: jax.tree_util.PyTreeDef,
         states: chex.Array,
         actions: chex.Array,
         target_qs: chex.Array,
     ):
-        q_values, variable_updates = q_fn(
-            heuristic_params, states, training=True, mutable=["batch_stats"]
-        )
-        heuristic_params["batch_stats"] = variable_updates["batch_stats"]
-        q_values_at_actions = jnp.take_along_axis(
-            q_values, actions[:, jnp.newaxis], axis=1
-        ).squeeze(1)
-        diff = target_qs - q_values_at_actions
-        loss = jnp.mean(optax.l2_loss(q_values_at_actions, target_qs))
-        return loss, (heuristic_params, diff)
+        q_values, variable_updates = q_fn(q_params, states, training=True, mutable=["batch_stats"])
+        new_params = {"params": q_params["params"], "batch_stats": variable_updates["batch_stats"]}
+        q_values_at_actions = jnp.take_along_axis(q_values, actions[:, jnp.newaxis], axis=1)
+        diff = target_qs.squeeze() - q_values_at_actions.squeeze()
+        loss = jnp.mean(jnp.square(diff))
+        return loss, (new_params, diff)
 
     def qlearning(
         key: chex.PRNGKey,
         dataset: tuple[chex.Array, chex.Array, chex.Array],
-        heuristic_params: jax.tree_util.PyTreeDef,
+        q_params: jax.tree_util.PyTreeDef,
         opt_state: optax.OptState,
     ):
         """
@@ -59,28 +55,26 @@ def qlearning_builder(
         batched_actions = jnp.take(actions, batch_indexs, axis=0)
 
         def train_loop(carry, batched_dataset):
-            heuristic_params, opt_state = carry
+            q_params, opt_state = carry
             states, target_q, actions = batched_dataset
-            (loss, (heuristic_params, diff)), grads = jax.value_and_grad(
-                qlearning_loss, has_aux=True
-            )(
-                heuristic_params,
+            (loss, (q_params, diff)), grads = jax.value_and_grad(qlearning_loss, has_aux=True)(
+                q_params,
                 states,
                 actions,
                 target_q,
             )
-            updates, opt_state = optimizer.update(grads, opt_state, params=heuristic_params)
-            heuristic_params = optax.apply_updates(heuristic_params, updates)
-            return (heuristic_params, opt_state), (loss, diff)
+            updates, opt_state = optimizer.update(grads, opt_state, params=q_params)
+            q_params = optax.apply_updates(q_params, updates)
+            return (q_params, opt_state), (loss, diff)
 
-        (heuristic_params, opt_state), (losses, diffs) = jax.lax.scan(
+        (q_params, opt_state), (losses, diffs) = jax.lax.scan(
             train_loop,
-            (heuristic_params, opt_state),
+            (q_params, opt_state),
             (batched_states, batched_target_q, batched_actions),
         )
         loss = jnp.mean(losses)
         mean_abs_diff = jnp.mean(jnp.abs(diffs))
-        return heuristic_params, opt_state, loss, mean_abs_diff, diffs
+        return q_params, opt_state, loss, mean_abs_diff, diffs
 
     return jax.jit(qlearning)
 
