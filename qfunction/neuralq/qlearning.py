88b613064634d4b2f5a62c1f205b8e3afcad5475
tinker495 <wjdrbtjr495@gmail.com>
2025-03-11 10:21:47 +0900

    only_double_dqn
    Former-commit-id: 8f30f6b6ed1d529e4a732c054174d4e6efb237fd
    Former-commit-id: fc37626643ac503e5addc875b51dd37e5700dd40

--- qfunction/neuralq/qlearning.py
+++ qfunction/neuralq/qlearning.py
@@ -218,8 +218,8 @@ def get_qlearning_dataset_builder(
     dataset_minibatch_size: int,
     using_hindsight_target: bool = True,
     using_triangular_target: bool = False,
-    weights_ratio: float = 100.0,
-    use_kde: bool = True,
+    weights_ratio: float = 0.0,
+    use_kde: bool = False,
     kde_bandwidth: float = 2.0,
 ):
     weights_lambda = shuffle_length / max(weights_ratio, 1e-5)
