8273eb3f72c6a90df6799aec99358d95242b324b
tinker495 <wjdrbtjr495@gmail.com>
2025-04-08 10:25:31 +0900

    remove decay

--- cli/train_commands/dist_train_command.py
+++ cli/train_commands/dist_train_command.py
@@ -57,10 +57,8 @@ def setup_optimizer(params: PyTree, steps: int, one_iter_size: int) -> optax.Opt
     )
 
     def optimizer_fn(learning_rate):
-        mask = {"params": True, "batch_stats": False}
         return optax.chain(
             optax.scale_by_adam(),  # optax.scale_by_adopt(),
-            optax.add_decayed_weights(1e-5, mask=mask),
             # optax.scale_by_trust_ratio(),
             optax.scale_by_learning_rate(learning_rate),
         )
