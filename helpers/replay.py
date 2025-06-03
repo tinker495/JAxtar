2f41a705cc0e6597926b5d4f23bea1f24aea8595
tinker495 <wjdrbtjr495@gmail.com>
2025-06-03 13:45:49 +0900

    update traj replay

--- helpers/replay.py
+++ helpers/replay.py
@@ -25,8 +25,8 @@ def init_trajectory_experience_replay(
     fake_timestep = {
         "solve_config": solve_config.default(),
         "state": state.default((sample_sequence_length + 1,)),
-        "cost": jnp.zeros((sample_sequence_length + 1,), dtype=jnp.float32),
-        "action": jnp.zeros((sample_sequence_length + 1,), dtype=jnp.uint8),
+        "cost": jnp.zeros((sample_sequence_length,), dtype=jnp.float32),
+        "action": jnp.zeros((sample_sequence_length,), dtype=jnp.uint8),
     }
     state = buffer.init(fake_timestep)
     return buffer, state
