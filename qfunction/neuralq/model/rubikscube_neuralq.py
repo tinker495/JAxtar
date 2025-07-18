52f34b9bbf47c15bbfb533632b7233df9c0aad8e
tinker495 <wjdrbtjr495@gmail.com>
2025-07-18 11:42:51 +0900

    add neighbor encoding

--- qfunction/neuralq/model/rubikscube_neuralq.py
+++ qfunction/neuralq/model/rubikscube_neuralq.py
@@ -10,17 +10,27 @@ from qfunction.neuralq.neuralq_base import NeuralQFunctionBase
 class RubiksCubeNeuralQ(NeuralQFunctionBase):
     base_xy: chex.Array  # The coordinates of the numbers in the puzzle
 
-    def __init__(self, puzzle: RubiksCube, **kwargs):
+    def __init__(self, puzzle: RubiksCube, neighbor_encoding: bool = False, **kwargs):
+        self.neighbor_encoding = neighbor_encoding
         super().__init__(puzzle, **kwargs)
 
     def pre_process(
         self, solve_config: RubiksCube.SolveConfig, current: RubiksCube.State
     ) -> chex.Array:
         current_flatten_face = current.unpacked.faces.flatten()  # (3,3,6) -> (54,)
-        # Create a one-hot encoding of the flattened face
         current_one_hot = jax.nn.one_hot(
             current_flatten_face, num_classes=6
         ).flatten()  # 6 colors in Rubik's Cube
+
+        if self.neighbor_encoding:
+            neighbors, _ = self.puzzle.get_neighbours(solve_config, current)
+            neighbors_flatten_face = jax.vmap(lambda x: x.unpacked.faces.flatten())(
+                neighbors
+            )  # (6, 3, 3, 6) -> (6, 54)
+            # Create a one-hot encoding of the flattened face
+            neighbors_one_hot = jax.nn.one_hot(neighbors_flatten_face, num_classes=6).flatten()
+            current_one_hot = jnp.concatenate([current_one_hot, neighbors_one_hot], axis=-1)
+
         if self.is_fixed:
             one_hots = current_one_hot
         else:
