4aaa03a868e0c592de798d1fe463b57d65d562f2
tinker495 <wjdrbtjr495@gmail.com>
2025-08-03 03:28:25 +0900

    pre_proc in train

--- cli/train_commands/dist_train_command.py
+++ cli/train_commands/dist_train_command.py
@@ -92,6 +92,7 @@ def davi(
         train_options.train_minibatch_size,
         heuristic_model,
         optimizer,
+        heuristic.pre_process,
         n_devices=n_devices,
         use_target_confidence_weighting=train_options.use_target_confidence_weighting,
     )
@@ -265,6 +266,7 @@ def qlearning(
         train_options.train_minibatch_size,
         qfunc_model,
         optimizer,
+        qfunction.pre_process,
         n_devices=n_devices,
         use_target_confidence_weighting=train_options.use_target_confidence_weighting,
     )
