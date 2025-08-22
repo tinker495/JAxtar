3c113ab8022bd5585b716ed787c21207ae0fad94
tinker495 <wjdrbtjr495@gmail.com>
2025-08-22 16:12:40 +0900

    add largebatch setting

--- config/train_presets.py
+++ config/train_presets.py
@@ -6,4 +6,9 @@ train_presets = {
         steps=int(1e4),
         update_interval=128,
     ),
+    "largebatch": DistTrainOptions(
+        dataset_batch_size=32768 * 256,
+        dataset_minibatch_size=32768,
+        train_minibatch_size=32768,
+    ),
 }
