4df980f56895ca49e5ff5f7965a7f4578c7f499e
tinker495 <wjdrbtjr495@gmail.com>
2025-09-23 18:40:46 +0900

    add heap dtype

--- heuristic/neuralheuristic/neuralheuristic_base.py
+++ heuristic/neuralheuristic/neuralheuristic_base.py
@@ -10,6 +10,7 @@ from puxle import Puzzle
 from heuristic.heuristic_base import Heuristic
 from neural_util.modules import (
     DEFAULT_NORM_FN,
+    HEAD_DTYPE,
     DTYPE,
     PreActivationResBlock,
     ResBlock,
@@ -57,7 +58,8 @@ class HeuristicBase(nn.Module):
             )(x, training)
         if self.resblock_fn == PreActivationResBlock:
             x = self.norm_fn(x, training)
-        x = nn.Dense(1, dtype=DTYPE, kernel_init=nn.initializers.normal(stddev=0.01))(x)
+        x = x.astype(HEAD_DTYPE)
+        x = nn.Dense(1, dtype=HEAD_DTYPE, kernel_init=nn.initializers.normal(stddev=0.01))(x)
         return x
 
 
