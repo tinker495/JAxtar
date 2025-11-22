e37797b62a6e4c8a88509bb0eadc1c2524333447
tinker495 <wjdrbtjr495@gmail.com>
2025-11-23 02:37:38 +0900

    add benchmarks

--- config/benchmark_registry.py
+++ config/benchmark_registry.py
@@ -1,7 +1,16 @@
-from puxle.benchmark import RubiksCubeDeepCubeABenchmark
+from puxle.benchmark import (
+    RubiksCubeDeepCubeABenchmark,
+    SlidePuzzleDeepCubeA24Benchmark,
+    SlidePuzzleDeepCubeA35Benchmark,
+    SlidePuzzleDeepCubeA48Benchmark,
+    SlidePuzzleDeepCubeABenchmark,
+)
 
-from heuristic.neuralheuristic import RubiksCubeNeuralHeuristic
-from qfunction.neuralq import RubiksCubeNeuralQ
+from heuristic.neuralheuristic import (
+    RubiksCubeNeuralHeuristic,
+    SlidePuzzleNeuralHeuristic,
+)
+from qfunction.neuralq import RubiksCubeNeuralQ, SlidePuzzleNeuralQ
 
 from .pydantic_models import BenchmarkBundle, EvalOptions, NeuralCallableConfig
 
@@ -15,6 +24,82 @@ benchmark_bundles: dict[str, BenchmarkBundle] = {
         q_function_nn_config=NeuralCallableConfig(
             callable=RubiksCubeNeuralQ,
             param_paths={"default": "qfunction/neuralq/model/params/rubikscube_3.pkl"},
+        ),
+        eval_options=EvalOptions(
+            batch_size=10000,
+            cost_weight=0.6,
+            pop_ratio=float("inf"),
+            num_eval=-1,
+            early_stop_patience=10,
+            early_stop_threshold=0.1,
+        ),
+    ),
+    "slide15-deepcubea": BenchmarkBundle(
+        benchmark=SlidePuzzleDeepCubeABenchmark,
+        heuristic_nn_config=NeuralCallableConfig(
+            callable=SlidePuzzleNeuralHeuristic,
+            param_paths={"default": "heuristic/neuralheuristic/model/params/n-puzzle_4.pkl"},
+        ),
+        q_function_nn_config=NeuralCallableConfig(
+            callable=SlidePuzzleNeuralQ,
+            param_paths={"default": "qfunction/neuralq/model/params/n-puzzle_4.pkl"},
+        ),
+        eval_options=EvalOptions(
+            batch_size=10000,
+            cost_weight=0.6,
+            pop_ratio=float("inf"),
+            num_eval=-1,
+            early_stop_patience=10,
+            early_stop_threshold=0.1,
+        ),
+    ),
+    "slide24-deepcubea": BenchmarkBundle(
+        benchmark=SlidePuzzleDeepCubeA24Benchmark,
+        heuristic_nn_config=NeuralCallableConfig(
+            callable=SlidePuzzleNeuralHeuristic,
+            param_paths={"default": "heuristic/neuralheuristic/model/params/n-puzzle_5.pkl"},
+        ),
+        q_function_nn_config=NeuralCallableConfig(
+            callable=SlidePuzzleNeuralQ,
+            param_paths={"default": "qfunction/neuralq/model/params/n-puzzle_5.pkl"},
+        ),
+        eval_options=EvalOptions(
+            batch_size=10000,
+            cost_weight=0.6,
+            pop_ratio=float("inf"),
+            num_eval=-1,
+            early_stop_patience=10,
+            early_stop_threshold=0.1,
+        ),
+    ),
+    "slide35-deepcubea": BenchmarkBundle(
+        benchmark=SlidePuzzleDeepCubeA35Benchmark,
+        heuristic_nn_config=NeuralCallableConfig(
+            callable=SlidePuzzleNeuralHeuristic,
+            param_paths={"default": "heuristic/neuralheuristic/model/params/n-puzzle_6.pkl"},
+        ),
+        q_function_nn_config=NeuralCallableConfig(
+            callable=SlidePuzzleNeuralQ,
+            param_paths={"default": "qfunction/neuralq/model/params/n-puzzle_6.pkl"},
+        ),
+        eval_options=EvalOptions(
+            batch_size=10000,
+            cost_weight=0.6,
+            pop_ratio=float("inf"),
+            num_eval=-1,
+            early_stop_patience=10,
+            early_stop_threshold=0.1,
+        ),
+    ),
+    "slide48-deepcubea": BenchmarkBundle(
+        benchmark=SlidePuzzleDeepCubeA48Benchmark,
+        heuristic_nn_config=NeuralCallableConfig(
+            callable=SlidePuzzleNeuralHeuristic,
+            param_paths={"default": "heuristic/neuralheuristic/model/params/n-puzzle_7.pkl"},
+        ),
+        q_function_nn_config=NeuralCallableConfig(
+            callable=SlidePuzzleNeuralQ,
+            param_paths={"default": "qfunction/neuralq/model/params/n-puzzle_7.pkl"},
         ),
         eval_options=EvalOptions(
             batch_size=10000,
