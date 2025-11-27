f50767cbed96047947c0d5c14afaf4e3ccc1fae5
tinker495 <wjdrbtjr495@gmail.com>
2025-11-27 22:50:23 +0900

    rm soft ud

--- config/train_presets.py
+++ config/train_presets.py
@@ -19,8 +19,7 @@ train_presets = {
     ),
     "diffusion_distance_warmup": DistTrainOptions(
         steps=int(1e4),
-        update_interval=16,
-        use_soft_update=True,
+        update_interval=128,
         use_diffusion_distance=True,
         use_diffusion_distance_warmup=True,
         diffusion_distance_warmup_steps=500,
