1b177db6c4b8fc7c16976c7f9856438312b01359
tinker495 <wjdrbtjr495@gmail.com>
2026-01-29 13:07:45 +0900

    ud annotate

--- JAxtar/annotate.py
+++ JAxtar/annotate.py
@@ -1,7 +1,9 @@
+import os
+
 import jax.numpy as jnp
 
 KEY_DTYPE = jnp.float16
 ACTION_DTYPE = jnp.uint8
-HASH_SIZE_MULTIPLIER = 2  # Multiplier for hash table size to reduce collision probability
-CUCKOO_TABLE_N = 2
+HASH_SIZE_MULTIPLIER = int(os.environ.get("JAXTAR_HASH_SIZE_MULTIPLIER", "2"))
+CUCKOO_TABLE_N = int(os.environ.get("JAXTAR_HASH_BUCKET_SIZE", "16"))
 MIN_BATCH_SIZE = 128
