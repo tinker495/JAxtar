83c777ae82a333c82c1b4822e2471413d82606ef
tinker495 <wjdrbtjr495@gmail.com>
2026-01-29 13:37:16 +0900

    update dupe mode

--- JAxtar/annotate.py
+++ JAxtar/annotate.py
@@ -6,4 +6,8 @@ KEY_DTYPE = jnp.float16
 ACTION_DTYPE = jnp.uint8
 HASH_SIZE_MULTIPLIER = int(os.environ.get("JAXTAR_HASH_SIZE_MULTIPLIER", "2"))
 CUCKOO_TABLE_N = int(os.environ.get("JAXTAR_HASH_BUCKET_SIZE", "16"))
+DEDUPE_MODE = os.environ.get(
+    "JAXTAR_DEDUPE_MODE", os.environ.get("XTRUCTURE_HASHTABLE_DEDUPE_MODE", "approx")
+)
+os.environ["XTRUCTURE_HASHTABLE_DEDUPE_MODE"] = DEDUPE_MODE
 MIN_BATCH_SIZE = 128
