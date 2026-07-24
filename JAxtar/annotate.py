b0f2ba5e396ad1ec2cc4a4b95350016aebec5dcc
tinker495 <wjdrbtjr495@gmail.com>
2026-07-24 21:03:26 +0900

    fix: correct default value for JAXTAR_HASH_SIZE_MULTIPLIER to 1

--- JAxtar/annotate.py
+++ JAxtar/annotate.py
@@ -6,7 +6,7 @@ import jax.numpy as jnp
 KEY_DTYPE = jnp.float16
 ACTION_DTYPE = jnp.uint8
 ACTION_PAD = jnp.array(jnp.iinfo(ACTION_DTYPE).max, dtype=ACTION_DTYPE)
-HASH_SIZE_MULTIPLIER = int(os.environ.get("JAXTAR_HASH_SIZE_MULTIPLIER", "2"))
+HASH_SIZE_MULTIPLIER = int(os.environ.get("JAXTAR_HASH_SIZE_MULTIPLIER", "1"))
 CUCKOO_TABLE_N = int(os.environ.get("JAXTAR_HASH_BUCKET_SIZE", "16"))
 DEDUPE_MODE = os.environ.get(
     "JAXTAR_DEDUPE_MODE", os.environ.get("XTRUCTURE_HASHTABLE_DEDUPE_MODE", "approx")
